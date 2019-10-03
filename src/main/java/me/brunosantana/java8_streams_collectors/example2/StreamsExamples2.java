5820d51cd5e316a6340fc6eba8d277f23a32b56e
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-10-03 18:38:59 -0300

    Add example that filters movies by rating

---
+++ src/main/java/me/brunosantana/java8_streams_collectors/example2/StreamsExamples2.java
@@ -0,0 +1,20 @@
+package me.brunosantana.java8_streams_collectors.example2;
+
+import java.util.List;
+
+import me.brunosantana.java8_streams_collectors.domain.Movie;
+import me.brunosantana.java8_streams_collectors.factories.ListFactory;
+
+public class StreamsExamples2 {
+
+	public static void main(String[] args) {
+		
+		List<Movie> movieList = ListFactory.getMovieList();
+
+		movieList.stream()
+			.filter(m -> m.getRating() > 9)
+			.forEach(System.out::println);
+
+	}
+
+}
