676419b4b85523c939d12dae66e4887bb41ca442
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-10-04 09:46:40 -0300

    Add an example which uses collect method to get a list

---
+++ src/main/java/me/brunosantana/java8_streams_collectors/example4/StreamsExamples4.java
@@ -0,0 +1,22 @@
+package me.brunosantana.java8_streams_collectors.example4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.brunosantana.java8_streams_collectors.domain.Movie;
+import me.brunosantana.java8_streams_collectors.factories.ListFactory;
+
+public class StreamsExamples4 {
+
+	public static void main(String[] args) {
+		
+		List<Movie> movieList = ListFactory.getMovieList();
+		
+		ArrayList<Movie> movieListFiltered = movieList.stream()
+			.filter(m -> m.isFeatureFilm())
+			.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+		
+		movieListFiltered.forEach(System.out::println);
+	}
+
+}
